ba40e494f633c850f5d9923ad107032d9d957e9f
SusejR <rodriguezbsusej@gmail.com>
2019-11-15 14:01:14 -0300

    primera version

--- src/cipher.js
+++ src/cipher.js
@@ -1,9 +1,72 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
-    
+  encode: (offset, txtCode) => {
+      let resultCode = "";
+
+    for ( let position=0; position < txtCode.length; position++) {
+
+         let txtAscii = txtCode.charCodeAt(position);
+          let code ="";
+
+          if (txtAscii >=65 && txtAscii <=90){
+             code = (txtAscii - 65 + parseInt(offset)) % 26 + 65;
+          }
+          if (txtAscii >=97 && txtAscii <=122){
+              code = (txtAscii - 97 + parseInt(offset)) % 26 + 97;
+          }
+          if (txtAscii >=48 && txtAscii <=57){
+              code = (txtAscii - 48 + parseInt(offset)) % 10 + 48;
+          }
+          if (txtAscii >=32 && txtAscii <=47){
+              code = (txtAscii - 32 + parseInt(offset)) % 16 + 32;
+          }
+          if (txtAscii >=58 && txtAscii <=64){
+              code = (txtAscii - 58 + parseInt(offset)) % 7 + 58;
+          }
+          if (txtAscii >=91 && txtAscii <=96){
+              code = (txtAscii - 91 + parseInt(offset)) % 6 + 91;
+          }
+         resultCode+= String.fromCharCode(code);
+      }
+      return resultCode;
+      
   },
-  decode: () => {
-    /* Acá va tu código */
+
+  decode: (offset, txtDcode) => {
+      let resultDecode = "";
+
+      for (let position=0; position < txtDcode.length; position++) {
+
+          let txtAscii = txtDcode.charCodeAt(position);
+          let dcode ="";
+         
+         if (txtAscii >=65 && txtAscii <=90) {
+             dcode = (txtAscii - 90 - parseInt(offset)) % 26 + 90;
+           }
+
+          if (txtAscii >=97 && txtAscii <=122){
+              dcode = (txtAscii - 122 - parseInt(offset)) % 26 + 122;
+          }
+
+          if (txtAscii >=48 && txtAscii <=57){
+              dcode = (txtAscii - 57 - parseInt(offset)) % 10 + 57;
+          }
+          if (txtAscii >=32 && txtAscii <=47){
+              dcode = (txtAscii - 47 - parseInt(offset)) % 16 + 47;
+          }
+          if (txtAscii >=58 && txtAscii <=64){
+              dcode = (txtAscii - 64 - parseInt(offset)) % 7 + 64;
+          }
+          if (txtAscii >=91 && txtAscii <=96){
+              dcode = (txtAscii - 96 - parseInt(offset)) % 6 + 96;
+          }
+          
+           resultDecode+= String.fromCharCode(dcode);
+      }
+      return resultDecode;
+      
+
   }
-};
+      
+  
+}
+
--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
-/*Acá va tu código*/
+
+    const nombre = document.getElementById("nombre");
+    const position = document.getElementById("position");
+    const txtEncode = document.getElementById("txtEncode");
+    const btnEncode = document.getElementById("btnEncode");
+    const btnDecode = document.getElementById("btnDecode");
+    const txtDecode = document.getElementById("txtDecode");
+    const btnClear = document.getElementById("btnClear");
+    
+
+    btnEncode.addEventListener("click", () => {
+        let offset = parseInt(document.getElementById("position").value);
+        let txtUser = document.getElementById("txtEncode").value;
+        let cipherTxt = window.cipher.encode(offset, txtUser);
+        txtDecode.value = cipherTxt;
+
+    });
+
+  btnClear.addEventListener("click",() =>{
+      nombre.value = "";
+      position.value = "";
+      txtEncode.value = "";
+      txtDecode.value = "";
+      });
+
+
+      btnDecode.addEventListener("click", () => {
+          let offset = parseInt(document.getElementById("position").value);
+          let txtUser = document.getElementById("txtEncode").value;
+          let decipherTxt = window.cipher.decode(offset, txtUser);
+          txtDecode.value = decipherTxt;
+          
+      });
+      
+
