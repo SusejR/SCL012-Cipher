251d91908ab053f69ff1069f77d1c968a2d0c402
SusejR <rodriguezbsusej@gmail.com>
2019-11-18 14:15:53 -0300

    agregando test numeros

--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -14,6 +14,18 @@ describe('cipher', () => {
       assert.equal(window.cipher.encode(33, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'), 'HIJKLMNOPQRSTUVWXYZABCDEFG');
     });
 
+    it('debería retornar "hijklmnopqrstuvwxyzabcdefg" para "abcdefghijklmnopqrstuvwxyz" con offset 33', () =>{
+      assert.equal(window.cipher.encode(33, 'abcdefghijklmnopqrstuvwxyz'), 'hijklmnopqrstuvwxyzabcdefg');
+    })
+
+    it('debería retornar "4567890123" para "0123456789" con offset 14', () => {
+      assert.equal(window.cipher.encode(14, '0123456789'), '4567890123');
+    })
+
+    it('debería retornar "%" para " " con offset 5', () => {
+      assert.equal(window.cipher.encode(5, ' '), '%');
+    })
+
   });
 
   describe('cipher.decode', () => {
@@ -25,6 +37,19 @@ describe('cipher', () => {
     it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offset 33', () =>{
       assert.equal(window.cipher.decode(33, 'HIJKLMNOPQRSTUVWXYZABCDEFG'), 'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
     });
+
+    it('debería retornar "abcdefghijklmnopqrstuvwxyz" para "hijklmnopqrstuvwxyzabcdefg" con offset 33', () => {
+      assert.equal(window.cipher.decode(33, 'hijklmnopqrstuvwxyzabcdefg'), 'abcdefghijklmnopqrstuvwxyz');
+    })
+
+    it('debería retornar "0123456789" para "4567890123" con offset de 14', () => {
+      assert.equal(window.cipher.decode(14, '4567890123'), '0123456789');
+    })
+
+    it('debería retornar " " para "%" con offset 5', () => {
+      assert.equal(window.cipher.decode(5, '%'), ' ');
   });
 
-});
+
+  });
+}); 
